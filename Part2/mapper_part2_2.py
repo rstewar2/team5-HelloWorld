bd59efb2c989eb3b3add975b81f6add41a63c659
rstewar2 <rstewar2@ksu.edu>
2020-07-08 17:04:16 -0500

    Add files via upload
    Current WIP for Part 2

---
+++ Part2/mapper_part2_2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# Let Hadoop know we want to use Python to execute our file
+
+from itertools import cycle
+import csv
+import sys
+# This module provides access to some variables used or 
+# maintained by the interpreter and to functions that interact strongly with the interpreter. It is always available.
+# https://docs.python.org/3/library/sys.html
+
+class mvMapper(object):
+# Turn our records into key value pairs
+	def __init__(self, infile=sys.stdin, separator=',', beta=0.85):
+    # Prepare three arguments, naming the object, what is our input file, and what is our seperator
+		self.infile = infile # Rename infile to know what infile we are referencing
+		self.separator = separator # Rename seperator to know what objects seperator we are referencing
+		self.beta = beta # Beta is a chosen constant to allow for random teleportation
+		self.vectorize = dict() # create a dictionary variable that is consistent with our other variable naming
+		self.e = 1.0/875713
+		#self.vectorize = {1: 9.0/24, 2: 5.0/24, 3: 5.0/24, 4:5.0/24}
+		
+		
+		with open('part-00000.txt') as vector:
+	        	for v in vector:
+	            		elements = v.split(self.separator)
+	            		source = int(elements[0].strip())
+	            		value = float(elements[1].strip())
+	            		self.vectorize[source] = value
+			
+
+	def map(self): # Here we will actually output our Key, Value pair for the Reducer to take in
+		reader = csv.reader(self.infile)		
+		for row in reader: # Begin looping through the object line by line up to the end of the vector file, which we know is four lines long
+			source = int(row[0]) # Set our Source pages to column 0
+			dest = int(row[1]) # Set our Destination pages to column 1
+			prob = float(row[2]) # Set our probability to column 2. We will need to find a way to create this in part 2.
+			self.emit(dest, self.beta * prob * self.vectorize[source] + (1 - self.beta) * self.e) # Output our Key, Value Pair, (Destination Page, Weighted Vector = 1/rows*probability path)
+
+	def emit(self, key, value): # Emmitting Formatting
+		sys.stdout.write('{0}{1}{2}\n'.format(key, self.separator, value))
+
+	"""
+	def status(self, message): # Basic Troubleshooting we may call later
+		sys.stderr.write("reporter:status:{0}\n".format(message))
+
+	def read(self): # Remoce trailing whitespace function
+		for line in self.infile:
+			yield line.rstrip()
+			
+	def __iter__(self): # Set Object as the iterator going through line by line through self that doesn't minus trailing white space
+		for line in self.read():
+			yield line
+	"""
+
+if __name__ == '__main__': # Makes sure that all code is executed at the bottom and make sure that script being imported to make sure that the code is completely done before handing it over to Hadoop, this allows for better debugging.
+	mvMapper().map()
---
+++ Part2/runmv3_1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Cleanup
+rm part-00000.txt
+hdfs dfs -rm -R /user/rstewar2/Project3/Google_v1
+hdfs dfs -rm -R /user/rstewar2/Project3/Google_vfinal
+
+# Run the first iteration
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.6.0-cdh5.13.0.jar \
+        -input /user/rstewar2/Project3/Google_M.csv \
+        -output /user/rstewar2/Project3/Google_v1 \
+        -file mapper_part2_1.py \
+        -file reducer_part2_1.py \
+	-file initial_V.txt \
+        -mapper "python mapper_part2_1.py" \
+        -reducer "python reducer_part2_1.py"
+
+# Retrieve the vector file
+hdfs dfs -get /user/rstewar2/Project3/Google_v1/part-00000
+chmod +x part-00000
+mv part-00000 part-00000.txt
+#hdfs dfs -put part-00000.txt /user/rstewar2/Project3
+
+# Run the second iteration
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.6.0-cdh5.13.0.jar \
+        -input /user/rstewar2/Project3/Google_M.csv \
+        -output /user/rstewar2/Project3/Google_vfinal \
+        -file mapper_part2_2.py \
+        -file reducer_part2_2.py \
+	-file part-00000.txt \
+        -mapper "python mapper_part2_2.py" \
+        -reducer "python reducer_part2_2.py"
